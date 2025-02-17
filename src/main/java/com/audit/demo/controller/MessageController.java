e59381ffe9acdc665abb6fc99ff2f2c19738cd18
Sergio Ismael Ayala Hernandez <sergio.ayala@sofka.local>
2025-02-17 12:38:26 -0500

    se agregan cambios a la consulta paginada

--- src/main/java/com/audit/demo/controller/MessageController.java
+++ src/main/java/com/audit/demo/controller/MessageController.java
@@ -4,7 +4,7 @@ import com.audit.demo.dto.PageResponse;
 import com.audit.demo.model.Message;
 import com.audit.demo.service.MessageService;
 import lombok.AllArgsConstructor;
-import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
@@ -17,13 +17,23 @@ public class MessageController {
     private final MessageService messageService;
 
     @GetMapping("/byIdEntidad/{idEntidad}")
-    public Mono<PageResponse<Message>> getMessagesByIdEntidad(@PathVariable String idEntidad, @RequestParam Pageable pageable) {
+    public Mono<PageResponse<Message>> getMessagesByIdEntidad(
+            @PathVariable String idEntidad,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size) {
+
+        Pageable pageable = PageRequest.of(page, size);
         return messageService.getMessagesByIdEntidad(idEntidad, pageable);
     }
 
     @GetMapping("/byRecurso/{recurso}")
-    public Mono<PageResponse<Message>> getMessagesByRecurso(@PathVariable String recurso, @RequestParam Pageable pageable) {
+    public Mono<PageResponse<Message>> getMessagesByRecurso(
+            @PathVariable String recurso,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size) {
+
+        Pageable pageable = PageRequest.of(page, size);
         return messageService.getMessagesByRecurso(recurso, pageable);
     }
-
 }
+
