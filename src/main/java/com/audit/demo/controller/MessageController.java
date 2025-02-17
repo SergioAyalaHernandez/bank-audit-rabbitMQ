956de9c9868cf40c5caef0334129c3f17a3e2976
Sergio Ismael Ayala Hernandez <sergio.ayala@sofka.local>
2025-02-17 12:21:03 -0500

    se corrige request param en la petición para audit

--- src/main/java/com/audit/demo/controller/MessageController.java
+++ src/main/java/com/audit/demo/controller/MessageController.java
@@ -6,10 +6,7 @@ import com.audit.demo.service.MessageService;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -20,12 +17,12 @@ public class MessageController {
     private final MessageService messageService;
 
     @GetMapping("/byIdEntidad/{idEntidad}")
-    public Mono<PageResponse<Message>> getMessagesByIdEntidad(@PathVariable String idEntidad, Pageable pageable) {
+    public Mono<PageResponse<Message>> getMessagesByIdEntidad(@PathVariable String idEntidad, @RequestParam Pageable pageable) {
         return messageService.getMessagesByIdEntidad(idEntidad, pageable);
     }
 
     @GetMapping("/byRecurso/{recurso}")
-    public Mono<PageResponse<Message>> getMessagesByRecurso(@PathVariable String recurso, Pageable pageable) {
+    public Mono<PageResponse<Message>> getMessagesByRecurso(@PathVariable String recurso, @RequestParam Pageable pageable) {
         return messageService.getMessagesByRecurso(recurso, pageable);
     }
 
