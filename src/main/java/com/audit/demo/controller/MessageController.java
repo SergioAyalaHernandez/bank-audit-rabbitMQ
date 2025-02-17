d92d83fb50d3752b91c560a6b035563645406fa3
Sergio Ismael Ayala Hernandez <sergio.ayala@sofka.local>
2025-02-17 13:18:13 -0500

    se agrega esquema de endpoint a messages

--- src/main/java/com/audit/demo/controller/MessageController.java
+++ src/main/java/com/audit/demo/controller/MessageController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
 @RestController
-@RequestMapping("/messages")
+@RequestMapping("/api/messages")
 @AllArgsConstructor
 public class MessageController {
 
